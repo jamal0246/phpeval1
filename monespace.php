cc09e053b2ef72b2d717f7904d84c8a954d1d200
jamal0246 <jamal_k@laposte.net>
2020-12-16 09:17:17 +0100

    Création syst session pour données utilisateur

--- monespace.php
+++ monespace.php
@@ -1,3 +1,18 @@
+<?php
+//var_dump($_SESSION);
+require_once "models/Utilisateur.php";
+$connect=unserialize($_SESSION["user"]);
+//var_dump($connect);
+
+// J'appelle la fonction "getPseudo()" pour recevoir le nom du pseudo connecté pour ensuite personnaliser la page
+$pseudo=$connect->getPseudo();
+
+?>
+
+
 <div style="background: rgb(187, 243, 187)">
+
         <h3>Mon espace </h3>
+
+        <p> Bonjour <?= $pseudo?> , vous êtes bien connecté.</p>
 </div>
